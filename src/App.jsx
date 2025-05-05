e6cf24257042cd1f8057e35550426ec1f35237f3
Avi Buskila <avrhahm18@gmail.com>
2025-05-05 19:24:12 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ 
+
+  return <>
+  <h1>this is jsx</h1>
+  console.log('App component rendered');
+
+  </>
+  
+
+   
+  
+}
+
+export default App
